89bc9d670c7a08718954de01971eb0bd11b67368
Mikko Sipola <N3120@student.jamk.fi>
2022-05-05 17:12:19 +0300

    2nd commit

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,5 +1,7 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+ import axios from 'axios'
+//const baseUrl = 'http://localhost:3001/api/notes'
+//const baseUrl = 'https://frozen-plains-28276.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
